c8bdcbead08031a7f461724c0f1f8d5ec0f187ef
Ibrahim Sangaré <1234567+isangare-ux@users.noreply.github.com>
2026-06-01 21:33:17 +0200

    Systeminformation im Support Check erweitert

--- support_check.sh
+++ support_check.sh
@@ -4,4 +4,7 @@ echo "Support Check gestartet"
 echo "Hostname: $(hostname)"
 echo "Aktueller Benutzer: $(whoami)"
 echo "Aktuelles Datum: $(date)"
+echo "Kernel-Version:" $(uname -r)
+echo "Betriebssystem:"
+cat /etc/os-release | grep PRETTY_NAME
 
